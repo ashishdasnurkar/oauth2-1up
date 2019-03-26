95ee729291cc3ba66ec4dd62c391610f8c6ca043
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2019-03-26 16:10:39 +0530

    completed code exchange /token endpoint

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const CLIENTS = [
-  { id: '1', "secret" : "secret1", "redirect_uris" :["https://ashishdasnurkar.com/callback"]},
+  { id: '1', "secret" : "secret1", "redirect_uris" :["https://thameera.com/callback"]},
 ]
 
-const AUTHZ_CODES = []
+let AUTHZ_CODES = []
 
+const ACCESS_TOKENS = []
 const getClientByID = id => {
   return CLIENTS.find(c => c.id === id)
 }
@@ -12,7 +13,22 @@ const saveCodeContext = ctx => {
   AUTHZ_CODES.push(ctx)
 }
 
+const getCodeContext = code => {
+  return AUTHZ_CODES.find(ctx => ctx.code === code)
+}
+
+const deleteCodeContext = code => {
+  AUTHZ_CODES = AUTHZ_CODES.filter(ctx => ctx.code !== code)
+}
+
+const saveAccessToken = ctx => {
+  ACCESS_TOKENS.push(ctx)
+}
+
 module.exports = {
   getClientByID,
-  saveCodeContext
+  saveCodeContext,
+  getCodeContext,
+  deleteCodeContext,
+  saveAccessToken
 }
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var moment = require('moment')
 
 
 const app = express();
+// app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
 app.set('view engine', 'ejs')
 
 
@@ -56,7 +59,7 @@ app.get("/authorize", (req, res) => {
     code,
     clientID,
     expiresAt,
-    redirectUri,
+    redirectUri:  req.query.redirect_uri,
     user_id: "1"
   }
 
@@ -69,6 +72,89 @@ app.get("/authorize", (req, res) => {
   // return res.status(501).render('error' , { "message" : "/authorize not fully implemented yet"})
   res.redirect(url)
 
+})
+
+app.post('/token', (req, res) => {
+  const body = req.body || {}
+  const grant = body.grant_type
+  if(!grant) {
+    return res.status(400).json({'error' : 'invalid_grant', 'error description': 'Missing grant type' })
+  }
+  if(grant != "authorization_code") {
+    return res.status(400).json({'error' : 'unsupported_grant_type', 'error_description': 'Unsupported grant type' })
+  }
+
+  const auth = req.headers["authorization"]
+  let client = null
+  if(auth) {
+    // basic auth
+    const parts = auth.trim().split(" ")
+    if(parts.length != 2 || parts[0].toLowerCase() !== 'basic' ) {
+      res.set("WWW-Authenticate", "Basic")
+      return res.status(401).send({"error" : "invalid_client", "error_description" : "Unsupported client authentication method"})
+    }
+
+    const creds = Buffer.from(parts[1], "base64").toString('ascii').split(":")
+    client = db.getClientByID(creds[0])
+
+    if(!client || client.secret !== creds[1]) {
+      res.set("WWW-Authenticate", "Basic")
+      return res.status(401).send({"error" : "invalid_client", "error_description" : "invalid client or secret"})
+    }
+  } else {
+    // json body auth
+    if(!body.client_id || !body.client_secret) {
+      return res.status(401).send({"error" : "invalid_client", "error_description" : "Client authentication failed"})
+    }
+    client = db.getClientByID(body.client_id)
+    if(!client || client.secret !== body.client_secret) {
+      res.set("WWW-Authenticate", "Basic")
+      return res.status(401).send({"error" : "invalid_client", "error_description" : "invalid client or secret"})
+    }
+  }
+
+  if(!body.code) {
+    return res.status(400).send({"error" : "invalid_request", "error_description" : "missing required parameter: code"})
+  }
+
+  const ctx = db.getCodeContext(body.code)
+
+  if(!ctx) {
+    return res.status(400).send({"error" : "invalid_grant", "error_description" : "Invalid authorization code"})
+  }
+
+  db.deleteCodeContext(body.code)
+
+
+  if(moment().isAfter(ctx.expiresAt)) {
+    return res.status(400).send({"error" : "invalid_grant", "error_description" : "Expired authorization code"})
+  }
+
+  if(ctx.clientID !== client.id) {
+    return res.status(400).send({"error" : "invalid_grant", "error_description" : "Invalid authorization code"})
+  }
+
+  if(ctx.redirectUri) {
+    if(body.redirect_uri !== ctx.redirectUri) {
+      return res.status(400).send({"error" : "invalid_grant", "error_description" : "Invalid redirect uri"})
+    }
+  }
+
+  const token = 'at-' + randomstring.generate({length: 32, charset: 'alphanumeric'})
+  const tokenCtx = {
+    token,
+    expiresIn: moment().add(120, 'minutes').valueOf(),
+    clientID: ctx.clientID,
+    userID: ctx.userID,
+  }
+  db.saveAccessToken(tokenCtx);
+  res.set('Cache-Control', 'no-store')
+  res.set('Pragma', 'no-cache')
+  res.status(200).json({
+    access_token: token,
+    token_type: "Bearer",
+    expires_in: 120 * 60
+  })
 })
 
 
